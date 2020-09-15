7bd22077d24f9c720779ffa75b6005b7c08e582c
ThomasFontier <thomas.fontier@polymtl.ca>
2020-09-15 22:51:03 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ def is_even_len(string: str) -> bool:
 def remove_third_char(string: str) -> str:
     for c in range(0, len(string), 1)
     if c != 3
-    print(string)
+    print(string[])
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
