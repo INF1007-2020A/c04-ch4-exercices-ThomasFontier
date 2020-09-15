32c93420313c9a48356523397df6e435995726a1
ThomasFontier <thomas.fontier@polymtl.ca>
2020-09-15 22:50:38 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,7 +10,8 @@ def is_even_len(string: str) -> bool:
 
 def remove_third_char(string: str) -> str:
     for c in range(0, len(string), 1)
-    if c 
+    if c != 3
+    
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
