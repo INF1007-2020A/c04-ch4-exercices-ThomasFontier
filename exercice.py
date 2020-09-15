a3deee0514f0ff36e4823443d0d9bcfbe4289496
ThomasFontier <thomas.fontier@polymtl.ca>
2020-09-15 23:43:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,8 +22,7 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 def get_number_of_char(string: str, char: str) -> int:
     for c in range(string):
-        
-        if (binaire == banaire_2):
+        if ()
             k += 1 
     return k
 
